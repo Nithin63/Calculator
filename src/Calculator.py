deff85fd1426afeb6a894a4439895bb0a4a2946c
Nithinreddy127 <nithinreddy127@gmail.com>
2020-11-01 14:56:48 -0500

    Feature: Setup addition and its Testing

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,5 +1,28 @@
+from CsvReader import CsvReader
+import math
+
+
+def addition(a, b):
+    a = int(a)
+    b = int(b)
+    c = a + b
+    return c
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+
+class CSVStats(Calculator):
+    data = []
+
+    def __init__(self, data_file):
+        self.data = CsvReader(data_file)
         pass
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -10,6 +10,12 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
+    def test_addition(self):
+        test_data = CsvReader('/src/TestFile/Addition.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
 
 if __name__ == '__main__':
     unittest.main()
